5ced286d3a97f9da53f59236b039114fa2b56dee
Jared Brown <jared@Jareds-MBP.lan1>
2019-12-28 02:10:28 -0800

    added text background color and changed font

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 import "./menu-item.styles.scss";
 
-const MenuItem = ({ title, imageUrl }) => (
+const MenuItem = ({ title, imageUrl, size }) => (
   <div
     style={{
       backgroundImage: `url(${imageUrl})`
     }}
-    className="menu-item"
+    className={`${size} menu-item`}
   >
     <div className="content">
       <div className="title">{title}</div>
