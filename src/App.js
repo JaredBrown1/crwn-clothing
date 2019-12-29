0a8b923146a54c66fcd58584a231251f69206367
Jared Brown <jared@Jareds-MBP.lan1>
2019-12-29 00:20:01 -0800

    added react router dom

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 
 import HomePage from "./components/pages/homepage/homepage.component";
 
 import "./App.css";
 
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
+
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <HomePage />
+      <div>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/hats" component={HatsPage} />
+        </Switch>
       </div>
     );
   }
