f6dda4c4b92ad1e3c9ff5a340061901012ac1825
Jared Brown <jaredbrown1@hotmail.com>
2021-03-30 16:21:02 -0700

    updating stripe integration and updating our favicon and title

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,40 +3,40 @@ import "firebase/firestore";
 import "firebase/auth";
 
 const config = {
-  apiKey: "AIzaSyCVj9Zcqt-_1woHiltRzs7xwZzN31Iolnc",
-  authDomain: "crwn-db-5d57c.firebaseapp.com",
-  databaseURL: "https://crwn-db-5d57c.firebaseio.com",
-  projectId: "crwn-db-5d57c",
-  storageBucket: "crwn-db-5d57c.appspot.com",
-  messagingSenderId: "786615837414",
-  appId: "1:786615837414:web:137af2750927286f6a1127",
-  measurementId: "G-T22RF944DH",
+	apiKey: "AIzaSyCVj9Zcqt-_1woHiltRzs7xwZzN31Iolnc",
+	authDomain: "crwn-db-5d57c.firebaseapp.com",
+	databaseURL: "https://crwn-db-5d57c.firebaseio.com",
+	projectId: "crwn-db-5d57c",
+	storageBucket: "crwn-db-5d57c.appspot.com",
+	messagingSenderId: "786615837414",
+	appId: "1:786615837414:web:137af2750927286f6a1127",
+	measurementId: "G-T22RF944DH",
 };
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
-  if (!userAuth) return;
+	if (!userAuth) return;
 
-  const userRef = firestore.doc(`users/${userAuth.uid}`);
+	const userRef = firestore.doc(`users/${userAuth.uid}`);
 
-  const snapShot = await userRef.get();
+	const snapShot = await userRef.get();
 
-  if (!snapShot.exists) {
-    const { displayName, email } = userAuth;
-    const createdAt = new Date();
+	if (!snapShot.exists) {
+		const { displayName, email } = userAuth;
+		const createdAt = new Date();
 
-    try {
-      await userRef.set({
-        displayName,
-        email,
-        createdAt,
-        ...additionalData,
-      });
-    } catch (error) {
-      console.log("error creating user", error.message);
-    }
-  }
+		try {
+			await userRef.set({
+				displayName,
+				email,
+				createdAt,
+				...additionalData,
+			});
+		} catch (error) {
+			console.log("error creating user", error.message);
+		}
+	}
 
-  return userRef;
+	return userRef;
 };
 
 firebase.initializeApp(config);
