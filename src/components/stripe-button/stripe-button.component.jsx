5c2acc55a5938d5229911948eaa0bfadd221b21f
Jared Brown <jaredbrown1@hotmail.com>
2021-05-23 17:53:39 -0700

    shop items bug fixed

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -1,30 +1,29 @@
-import React from "react";
-import StripeCheckout from "react-stripe-checkout";
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
 
 const StripeCheckoutButton = ({ price }) => {
-	const priceForStripe = price * 100;
-	const publishableKey =
-		"pk_test_51IaqH2JDvWJZUcGlREarMGaA4f1oI04vgEsykd7d3dCKEQVqocJ8tRavLVP13DfEj7QuH30oowrG3oehpeAV8b7r00h5ZuAoP2";
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_WBqax2FWVzS9QlpJScO07iuL';
 
-	const onToken = (token) => {
-		console.log(token);
-		alert("Payment Succesful!");
-	};
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Succesful!');
+  };
 
-	return (
-		<StripeCheckout
-			label="Pay Now"
-			name="CRWN Clothing Ltd."
-			billingAddress
-			shippingAddress
-			image="https://svgshare.com/i/CUz.svg"
-			description={`Your total is $${price}`}
-			amount={priceForStripe}
-			panelLabel="Pay Now"
-			token={onToken}
-			stripeKey={publishableKey}
-		/>
-	);
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='CRWN Clothing Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
 };
 
 export default StripeCheckoutButton;
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,84 +1,82 @@
-import firebase from "firebase/app";
-import "firebase/firestore";
-import "firebase/auth";
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
 
 const config = {
-	apiKey: "AIzaSyCVj9Zcqt-_1woHiltRzs7xwZzN31Iolnc",
-	authDomain: "crwn-db-5d57c.firebaseapp.com",
-	databaseURL: "https://crwn-db-5d57c.firebaseio.com",
-	projectId: "crwn-db-5d57c",
-	storageBucket: "crwn-db-5d57c.appspot.com",
-	messagingSenderId: "786615837414",
-	appId: "1:786615837414:web:137af2750927286f6a1127",
-	measurementId: "G-T22RF944DH",
+  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
+  authDomain: 'crwn-db.firebaseapp.com',
+  databaseURL: 'https://crwn-db.firebaseio.com',
+  projectId: 'crwn-db',
+  storageBucket: 'crwn-db.appspot.com',
+  messagingSenderId: '850995411664',
+  appId: '1:850995411664:web:7ddc01d597846f65'
 };
 
 firebase.initializeApp(config);
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
-	if (!userAuth) return;
+  if (!userAuth) return;
 
-	const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
 
-	const snapShot = await userRef.get();
+  const snapShot = await userRef.get();
 
-	if (!snapShot.exists) {
-		const { displayName, email } = userAuth;
-		const createdAt = new Date();
-		try {
-			await userRef.set({
-				displayName,
-				email,
-				createdAt,
-				...additionalData,
-			});
-		} catch (error) {
-			console.log("error creating user", error.message);
-		}
-	}
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
 
-	return userRef;
+  return userRef;
 };
 
 export const addCollectionAndDocuments = async (
-	collectionKey,
-	objectsToAdd
+  collectionKey,
+  objectsToAdd
 ) => {
-	const collectionRef = firestore.collection(collectionKey);
+  const collectionRef = firestore.collection(collectionKey);
 
-	const batch = firestore.batch();
-	objectsToAdd.forEach((obj) => {
-		const newDocRef = collectionRef.doc();
-		console.log("firebase utils", newDocRef);
-		batch.set(newDocRef, obj);
-	});
+  const batch = firestore.batch();
+  objectsToAdd.forEach(obj => {
+    const newDocRef = collectionRef.doc();
+    batch.set(newDocRef, obj);
+  });
 
-	return await batch.commit();
+  return await batch.commit();
 };
 
-export const convertCollectionsSnapshotToMap = (collections) => {
-	const transformedCollection = collections.docs.map((doc) => {
-		const { title, items } = doc.data();
+export const convertCollectionsSnapshotToMap = collections => {
+  const transformedCollection = collections.docs.map(doc => {
+    const { title, items } = doc.data();
 
-		return {
-			routeName: encodeURI(title.toLowerCase()),
-			id: doc.id,
-			title,
-			items,
-		};
-	});
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items
+    };
+  });
 
-	return transformedCollection.reduce((accumulator, collection) => {
-		accumulator[collection.title.toLowerCase()] = collection;
-		return accumulator;
-	}, {});
+  return transformedCollection.reduce((accumulator, collection) => {
+    accumulator[collection.title.toLowerCase()] = collection;
+    return accumulator;
+  }, {});
 };
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: "select_account" });
+provider.setCustomParameters({ prompt: 'select_account' });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
